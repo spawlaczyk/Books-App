21e9abcb06334518600f2480dc9d58d43fa6ebd4
Stanisław Pawlaczyk <ctahncnab@gmail.com>
2022-02-02 13:31:32 +0100

    Add favoriteBooks/dbclick problem

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,49 @@
 {
-    'use strict';
+  'use strict';
 
-    const select = {
-        templateOf: {
-            book: '#template-book',
-        },
-        listOf: {
-            booksList: '.books-list',
-        }
-    };
+  const select = {
+    templateOf: {
+      book: '#template-book',
+    },
+    containerOf: {
+      booksList: '.books-list',
+      bookImage: '.book__image',
+    },
+  };
 
-    const templates = {
-        bookTemplate: Handlebars.compile(document.querySelector(select.templateOf.book).innerHTML),
-    };
+  const templates = {
+    bookTemplate: Handlebars.compile(document.querySelector(select.templateOf.book).innerHTML),
+  };
 
-    const render = function () {
-        for (let book of dataSource.books) {
-            const generatedHTML = templates.bookTemplate(book);
-            const elementDOM = utils.createDOMFromHTML(generatedHTML);
-            const booksContainer = document.querySelector(select.listOf.booksList);
-            booksContainer.appendChild(elementDOM);
-        }
-    };
+  function render() {
+    for (let book of dataSource.books) {
+      const generatedHTML = templates.bookTemplate(book);
 
-    render();
+      const elementDOM = utils.createDOMFromHTML(generatedHTML);
+
+      const booksContainer = document.querySelector(select.containerOf.booksList);
+
+      booksContainer.appendChild(elementDOM);
+    }
+  }
+
+  render();
+
+  const favoriteBooks = [];
+
+  function initActions(){
+    const images = document.querySelectorAll(select.containerOf.bookImage);
+
+    for(let image of images){
+      image.addEventListener('dbclick', function(event){
+        event.preventDefault();
+        image.classList.add('favorite');
+        let bookId = image.getAttribute('data-id');
+        favoriteBooks.push(bookId);
+        console.log('favoriteBooks:', favoriteBooks);
+      });
+    }
+  }
+
+  initActions();
 }
