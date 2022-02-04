6e7456e5c25839968c4a7b04759421790e23ab1c
Stanisław Pawlaczyk <ctahncnab@gmail.com>
2022-02-04 14:48:51 +0100

    Add rating

--- src/js/script.js
+++ src/js/script.js
@@ -21,6 +21,12 @@
 
   function render() {
     for (let book of dataSource.books) {
+      const ratingBgc = determineRatingBgc(book.rating);
+
+      book.ratingBgc = ratingBgc;
+
+      book.ratingWidth = book.rating * 10;
+
       const generatedHTML = templates.bookTemplate(book);
 
       const elementDOM = utils.createDOMFromHTML(generatedHTML);
@@ -87,6 +93,22 @@
         bookToShow.classList.remove('hidden');
       }
     }
+  }
+
+  function determineRatingBgc(rating){
+    let background;
+
+    if(rating < 6){
+      background = 'linear-gradient(to bottom, #fefcea 0%, #f1da36 100%)';
+    } else if (rating > 6 && rating <= 8){
+      background = 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%)';
+    } else if (rating > 8 && rating <= 9) {
+      background = 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
+    } else if (rating > 9) {
+      background = 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
+    }
+
+    return background;
   }
 
   render();
